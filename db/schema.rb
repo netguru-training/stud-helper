210b4079c6bedec77ea360bd54705acb0e8a9acc
Katarzyna Jaskólska <katarzynaj846@gmail.com>
2015-04-25 14:01:03 +0200

    Add migration for user's picture

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425114334) do
+ActiveRecord::Schema.define(version: 20150425115921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20150425114334) do
     t.datetime "updated_at"
     t.string   "name"
     t.integer  "role"
+    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
