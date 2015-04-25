66ee1a961c0451622142d987c4e4e08bb19b3c4a
Katarzyna Jaskólska <katarzynaj846@gmail.com>
2015-04-25 15:04:07 +0200

    Add some configuration to permit users picture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
 
   protected
+
   def check_if_admin!
     unless current_user.admin?
       flash[:error] = "You don't have admin permission."
@@ -18,4 +21,9 @@ class ApplicationController < ActionController::Base
   def already_voted(resource)
     flash[:danger] = "Sorry, you had already voted this #{resource.class.name}"
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) << :picture
+  end
+
 end
