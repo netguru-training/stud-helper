d5306a2358e3d1d1483489ae2196bf2b27872c68
Katarzyna Jaskólska <katarzynaj846@gmail.com>
2015-04-25 12:57:05 +0200

    Add spec for subject_decorator full_name

--- spec/decorators/subject_decorator_spec.rb
+++ spec/decorators/subject_decorator_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe SubjectDecorator do
+  subject { described_class.new(subject_item) }
+
+  describe "#full_name" do
+    let(:subject_item) { build :subject, name: 'Math', short_name: 'MLO' }
+
+    it { expect(subject.full_name).to eq("Math (MLO)") }
+  end
 end
