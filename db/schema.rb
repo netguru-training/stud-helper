e1fadf3680727eea067d95f97de66dfe04ccb1c3
Aleksander Matusiak <matusiak.aleksander@gmail.com>
2015-04-25 11:59:13 +0200

    Rename comment owner

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425093619) do
+ActiveRecord::Schema.define(version: 20150425095359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: true do |t|
     t.string   "content"
-    t.integer  "commentable_id"
-    t.string   "commentable_type"
+    t.integer  "owner_id"
+    t.string   "owner_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
