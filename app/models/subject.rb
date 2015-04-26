092c7e8b8860ad84a08af0c4acebc3c4cbe85432
Justyn Jozwiak <jstn011@gmail.com>
2015-04-26 11:42:39 +0200

    added searching with prefix

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,6 +1,6 @@
 class Subject < ActiveRecord::Base
 	include PgSearch
-	pg_search_scope :search_by_short_name, against: :short_name
+	pg_search_scope :search_by_short_name, against: :short_name, :using => {:tsearch => {:prefix => true}}
 
 	has_and_belongs_to_many :professors
 	has_many :subject_items
