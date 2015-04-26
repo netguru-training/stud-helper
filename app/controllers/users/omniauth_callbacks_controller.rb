c4e31cd14ffb3f5e2e5d164071dd11e0716e9df4
Aleksander Matusiak <matusiak.aleksander@gmail.com>
2015-04-26 09:12:33 +0200

    Fix facebook signup

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -29,7 +29,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     #@auth.first_name
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    #@user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
